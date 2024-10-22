71db14dae0686c1b9824f1d81f0436b3f8f78eb1
Anis <anmounsi01@gmail.com>
2024-10-22 19:42:54 +0200

    Update championMastery endpoints

--- Endpoints/championMastery_v4.py
+++ Endpoints/championMastery_v4.py
@@ -4,10 +4,8 @@ class ChampionMasteryApi:
     ENDPOINTS = {
         'BY_PUUID': '/lol/champion-mastery/v4/champion-masteries/by-puuid/{}',
         'BY_PUUID_CHAMPION': '/lol/champion-mastery/v4/champion-masteries/by-puuid/{}/by-champion/{}',
-        'BY_SUMMONER': '/lol/champion-mastery/v4/champion-masteries/by-summoner/{}',
-        'BY_SUMMONER_CHAMPION': '/lol/champion-mastery/v4/champion-masteries/by-summoner/{}/by-champion/{}',
-        'BY_SUMMONER_TOP_CHAMPIONS': '/lol/champion-mastery/v4/champion-masteries/by-summoner/{}/top',
-        'BY_SUMMONER_TOTAL_SCORE': '/lol/champion-mastery/v4/scores/by-summoner/{}'
+        'BY_PUUID_TOP_CHAMPIONS': '/lol/champion-mastery/v4/champion-masteries/by-puuid/{}/top',
+        'BY_PUUID_SUM_SCORE': '/lol/champion-mastery/v4/scores/by-puuid/{}',
     }
 
     def __init__(self, region, api_key):
@@ -19,19 +17,10 @@ class ChampionMasteryApi:
     def by_puuid_champion(self, puuid, champion_id):
         return self.request_handler.make_request(self.ENDPOINTS['BY_PUUID_CHAMPION'].format(puuid, champion_id))
 
-    def by_summoner(self, summoner_id):
-        return self.request_handler.make_request(self.ENDPOINTS['BY_SUMMONER'].format(summoner_id))
-    
-    def champ_mastery(self, summoner_id, champion_id):
-        return self.request_handler.make_request(self.ENDPOINTS['BY_SUMMONER_CHAMPION'].format(summoner_id, champion_id))
-    
-    def top_champs(self, summoner_id, count=5):
+    def by_puuid_top_champions(self, summoner_id, count=3):
         query_params = {k: v for k, v in locals().items() if v is not None and k != 'self'}
-
-        return self.request_handler.make_request(self.ENDPOINTS['BY_SUMMONER_TOP_CHAMPIONS'].format(summoner_id), query_params=query_params)
+        return self.request_handler.make_request(self.ENDPOINTS['BY_PUUID_TOP_CHAMPIONS'].format(summoner_id))
     
-    def total_score(self, summoner_id):
-        return self.request_handler.make_request(self.ENDPOINTS['BY_SUMMONER_TOTAL_SCORE'].format(summoner_id))
-
-# create new function
+    def by_puuid_sum_score(self, summoner_id, champion_id):
+        return self.request_handler.make_request(self.ENDPOINTS['BY_PUUID_SUM_SCORE'].format(summoner_id, champion_id))
     
